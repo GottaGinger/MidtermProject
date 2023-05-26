e3e1b52b1633eafbb6a79daa2182333256166490
GottaGinger <114538296+GottaGinger@users.noreply.github.com>
2023-05-25 22:02:00 -0400

    commit
    Sierra 1st commit

---
+++ Midterm/Midterm/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Midterm;
+
+//unsure
+Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", "Checked out", DateTime.Now); //due date is two weeks from the checkout date
+Book book2 = new Book("1984", "George Orwell", "On Shelf ", DateTime.Now);
+Book book3 = new Book("Pride and Prejudice", "Jane Austen", "On Shelf ", DateTime.Now);
+Book book4 = new Book("The Catcher in the Rye", "J.D. Salinger", "On Shelf ", DateTime.Now);
+Book book5 = new Book("The Alchemist", "Paulo Coelho", "On Shelf ", DateTime.Now);
+Book book6 = new Book("The Book Thief", "Markus Zusak", "On Shelf ", DateTime.Now);
+Book book7 = new Book("The Girl on the Train", "Paula Hawkins", "On Shelf ", DateTime.Now);
+Book book8 = new Book("Gone Girl", "Gillian Flynn", "On Shelf ", DateTime.Now);
+Book book9 = new Book("The Color Purple", "Alice Walker", "On Shelf ", DateTime.Now);
+Book book10 = new Book("The Picture of Dorian Gray", "Oscar Wilde", "On Shelf ", DateTime.Now);
+Book book11 = new Book("The Hobbit", "J.R.R. Tolkien", "Checked Out ", DateTime.Now);
+Book book12 = new Book("The Handmaids Tale", "Margaret Atwood", "On Shelf ", DateTime.Now);
+
+
